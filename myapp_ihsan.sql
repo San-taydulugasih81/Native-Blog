50370452a3d5f595fd55dfd96e9b707fb2929a99
San-taydulugasih81 <141897080+San-taydulugasih81@users.noreply.github.com>
2023-10-18 13:44:05 +0700

    Add files via upload

---
+++ myapp_ihsan.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2023 at 08:34 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `myapp_ihsan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hero`
+--
+
+CREATE TABLE `hero` (
+  `idHero` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `subjudul` text NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `hero`
+--
+
+INSERT INTO `hero` (`idHero`, `gambar`, `judul`, `subjudul`, `status`) VALUES
+(6, 'Kali Linux.jpg', 'Learn Kali Linux', 'Belajar Ethical Hacking untuk menjadi Cyber Security', 'aktif'),
+(7, 'html.png', 'Learn HTML', 'Belajar HTML Dasar untuk menjadi Front End Developer', 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `idUser` varchar(20) NOT NULL,
+  `nmUser` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`idUser`, `nmUser`, `username`, `password`, `level`) VALUES
+('1', 'Ihsan Fadlurrahman', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hero`
+--
+ALTER TABLE `hero`
+  ADD PRIMARY KEY (`idHero`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hero`
+--
+ALTER TABLE `hero`
+  MODIFY `idHero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
